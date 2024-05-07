7153ba81d71782fb806866f1a8e875bfbf5d64aa
rshal <heiphin7and2@gmail.com>
2024-05-07 23:06:31 +0500

    Added selenium using for download javascript code in page for parse

--- youtube-parser-service/src/main/java/heiphin/parser/service/YouTubeParser.java
+++ youtube-parser-service/src/main/java/heiphin/parser/service/YouTubeParser.java
@@ -4,34 +4,52 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.IOException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class YouTubeParser {
 
     public static void main(String[] args) {
-        String channelUrl = "https://www.youtube.com/@java/videos";
+        String channelUrl = "https://www.youtube.com/@requ2093/videos";
+        WebDriver webDriver = new ChromeDriver();
+
         try {
-            // Получаем HTML-код страницы канала
-            Document doc = Jsoup.connect(channelUrl).get();
+            webDriver.get(channelUrl);
+
+            // Ждем, пока элемент с определенным селектором не станет видимым
+            WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofMillis(10000)); // Максимальное время ожидания в секундах
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".style-scope.ytd-rich-grid-row")));
+
+            // После того, как элемент станет видимым, продолжаем выполнение кода
+            String html = webDriver.getPageSource();
+            System.out.println(html);
+
+            // Создаем объект Document из HTML-кода страницы
+            Document doc = Jsoup.parse(html);
 
             // Извлекаем блоки с видео
-            Elements videoElements = doc.select("div.yt-lockup-video");
+            Elements videoElements = doc.select("div.video-skeleton");
 
             // Проверяем, сколько элементов найдено
             System.out.println("Найдено видео: " + videoElements.size());
 
             // Перебираем каждый элемент с видео и извлекаем информацию
             for (Element videoElement : videoElements) {
-                System.out.println("цикл начался");
                 // Извлекаем заголовок видео
-                Element titleElement = videoElement.selectFirst("h3.yt-lockup-title a");
+                Element titleElement = videoElement.selectFirst(".title");
                 String title = titleElement.text();
 
                 // Извлекаем ссылку на видео
                 String videoUrl = "https://www.youtube.com" + titleElement.attr("href");
 
                 // Извлекаем количество просмотров
-                Element viewCountElement = videoElement.selectFirst("ul.yt-lockup-meta-info li");
+                Element viewCountElement = videoElement.selectFirst(".video-meta");
                 String viewCount = viewCountElement.text();
 
                 // Выводим информацию о видео
@@ -40,10 +58,11 @@ public class YouTubeParser {
                 System.out.println("Views: " + viewCount);
                 System.out.println();
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            webDriver.quit();
         }
     }
 }
-
 
