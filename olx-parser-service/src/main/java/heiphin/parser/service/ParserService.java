311797644e2f64febbfc61f37f7ba16cdbf8a0ba
rshal <heiphin7and2@gmail.com>
2024-05-08 19:33:19 +0500

    Added using selenium for download scripts for html page for parse

--- olx-parser-service/src/main/java/heiphin/parser/service/ParserService.java
+++ olx-parser-service/src/main/java/heiphin/parser/service/ParserService.java
@@ -4,9 +4,13 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ParserService {
@@ -15,29 +19,50 @@ public class ParserService {
     public void parseKolesa(String query) {
         String FULL_URL = BASE_URL + query + "/";
 
+        // Создаем экземпляр WebDriver
+        WebDriver webDriver = new ChromeDriver();
+
         try {
-            // Лишь после инициализации ссылки для текущей страницы инициализируем остальные переменные
-            Document doc = Jsoup.connect(FULL_URL).get();
+            // Устанавливаем максимальное время ожидания элементов на странице
+            webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+            // Загружаем страницу
+            webDriver.get(FULL_URL);
+
+            // Выполняем скрипты на странице для прокрутки вниз
+            JavascriptExecutor js = (JavascriptExecutor) webDriver;
+            js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+
+            // Ждем некоторое время для полной загрузки страницы
+            Thread.sleep(2000);
+
+            // Получаем HTML-код страницы
+            String html = webDriver.getPageSource();
+
+            // Создаем объект Document из HTML-кода страницы
+            Document doc = Jsoup.parse(html);
+
+            // Извлекаем объявления
             Elements carAds = doc.select("div.css-1sw7q4x");
 
-
+            // Перебираем каждое объявление и извлекаем информацию
             for (Element ad : carAds) {
-                Element titleElem = ad.selectFirst("h6.css-16v5mdi er34gjf0");
+                Element titleElem = ad.selectFirst("h6.css-16v5mdi.er34gjf0");
                 String title = (titleElem != null) ? titleElem.text().trim() : "Нет информации о названии";
 
                 Element linkElem = ad.selectFirst("a.css-z3gu2d");
                 String link = (linkElem != null) ? BASE_URL + linkElem.attr("href") : "Нет ссылки";
 
-                Element descriptionElem = ad.selectFirst("p.css-tyui9s er34gjf0");
+                Element descriptionElem = ad.selectFirst("p.css-tyui9s.er34gjf0");
                 String price = (descriptionElem != null) ? descriptionElem.text().trim() : "Нет описания";
 
-                Element locationElem = ad.selectFirst("p.css-1a4brun er34gjf0");
-                String locationInfo = (locationElem != null) ? locationElem.text().trim() : "Нет местонахождение";
+                Element locationElem = ad.selectFirst("p.css-1a4brun.er34gjf0");
+                String locationInfo = (locationElem != null) ? locationElem.text().trim() : "Нет местонахождения";
 
                 if ( // Если нет информации, то ничего не выводим
                         title.equals("Нет информации о названии") &&
                                 link.equals("Нет ссылки") &&
-                                    price.equals("Нет описания")
+                                price.equals("Нет описания")
                 ) {
                     continue;
                 }
@@ -48,9 +73,12 @@ public class ParserService {
                 System.out.println(locationInfo);
                 System.out.println("====================================");
 
-                }
-        } catch (IOException e) {
+            }
+        } catch (Exception e) {
             System.out.println(e.getMessage());
+        } finally {
+            // Закрываем браузер после завершения работы
+            webDriver.quit();
         }
     }
 }
