8b24d0327624302d97db72b396be30871893c70b
rshal <heiphin7and2@gmail.com>
2024-05-13 12:50:29 +0500

    changed application yaml & dockerfile to docker - kafka

--- kolesa-parser-service/src/main/java/heiphin/kolesa/KolesaParserApplication.java
+++ kolesa-parser-service/src/main/java/heiphin/kolesa/KolesaParserApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KolesaParserApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KolesaParserApplication.class, args);
 	}
-
 }
